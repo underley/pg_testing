03c79339a1e67b631663a9ad6345daa47033f5fb
Daniel Podlejski <daniel.podlejski@lft.pl>
2014-09-03 14:41:43 +0200

    django settings example

---
+++ django_setting.py
@@ -0,0 +1,11 @@
+DATABASES = { 
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': '127.0.0.1',
+        'NAME': 'tests',
+        'USER': 'postgres',
+        'PORT': 5499,
+        'PASSWORD': '', 
+        'ATOMIC_REQUESTS': True,
+    }
+}   
